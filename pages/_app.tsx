de83378c18e7d59947a096a00dc5927d63c3b334
mac-nick <quocngu4616@gmail.com>
2023-03-03 08:47:04 +0700

    add collection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
-import '../styles/globals.scss';
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import React from "react";
+import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
-
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import "../public/css/range.css";
+import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Script src="/js/icons.js"></Script>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export default dynamic(() => Promise.resolve(MyApp), {
